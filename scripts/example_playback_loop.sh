a54c94095cb9fd6ce5f7ad8cfd77b457cb48d946
Nick Jourjine <jourjine.nicholas@gmail.com>
2025-06-05 13:02:17 -0400

    first commit

--- scripts/example_playback_loop.sh
+++ scripts/example_playback_loop.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Plays an audio file 10 times with 10 minute intervals between each playback, after waiting 10 minutes.
+# Replace /path/to/your_ultrasound_recording.wav with the path to your own audio file.
+
 sleep 600  # Wait 10 minutes (600 seconds)
 
 for i in {1..10}
 do
-    play /media/jourjine/STICK/example_playback_condition.wav 
+    play /path/to/your_ultrasound_recording.wav 
     if [ $i -lt 10 ]; then
         sleep 600  # Wait 10 minutes (600 seconds) before the next playback, except after the last one
     fi
--- scripts/example_sensitivity_test.sh
+++ scripts/example_sensitivity_test.sh
@@ -7,4 +7,5 @@ freqs=(10000 20000 30000 40000 50000 60000 70000 80000 90000)
 for freq in "${freqs[@]}"; do
     echo "Playing tone at ${freq} Hz"
     play -V -r 192000 -n -b 16 -c 2 synth 2 sin "$freq" vol -10dB
+	sleep 1
 done
